ea65b1fe1b0f2417aa38731c2d08aec42820be19
DalitsoKasonde <dkasonde11@gmail.com>
2020-11-20 20:55:57 +0200

    final final

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // Imports
 
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
 const app = express();
 const port = 3000;
 
@@ -8,23 +10,23 @@ const port = 3000;
 // static files 
 app.use(express.static('public'));
 app.use('/css',express.static(__dirname + 'public/css'));
-app.use('/js',express.static(__dirname + 'public/js'));
-app.use('/img',express.static(__dirname + 'public/img'));
 
-// Set Views 
-app.set('views', './views');
+// Set Views without express layout
+// app.set('views', './views');
+
+// Set templating engine 
+app.use(expressLayouts);
+app.set('layout', './layouts/full-width');
 app.set('view engine', 'ejs');
 
+// Navigation 
+
 app.get('', (req, res) => {
-    res.render('index', {text: 'This is EJS'});
-    // res.send('Hello world 123');
-    // res.sendFile(__dirname + '/views/index.html');
+    res.render('index', { title: 'Home page' } );
 });
 
 app.get('/about', (req, res) => {
-    res.render('about', {text: 'This is About'});
-    // res.send('Hello world 123');
-    // res.sendFile(__dirname + '/views/index.html');
+    res.render('about', { title: 'About' , layout: './layouts/sidebar'});
 });
 
 // Listen on port 3000 
